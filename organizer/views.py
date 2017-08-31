d8456cfa476f0da63bd9d56d08ea1f53319e0676
Sal Gomez <svtgomez@gmail.com>
2017-08-30 19:51:55 -0700

    finished chapter 4

--- organizer/views.py
+++ organizer/views.py
@@ -1,10 +1,18 @@
 #from django.shortcuts import render
 from django.http.response import HttpResponse
+from django.template import Context, loader
 
 from .models import Tag
 
+
 def homepage(request):
     tag_list = Tag.objects.all()
-    output = ",".join([tag.name for tag in tag_list])
+    #output = ",".join([tag.name for tag in tag_list])
 
-    return HttpResponse('Hello (again) world!\nTags: {}'.format(output))
+    context = {'tag_list': tag_list}
+    #context = Context({'tag_list': tag_list})  # Book wants this, but it's borked
+    template = loader.get_template('organizer/tag_list.html')
+
+    output = template.render(context)
+
+    return HttpResponse(output)
