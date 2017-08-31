9e06749bb4a345b89ff2e7cab31295f70ab08ad4
Sal Gomez <svtgomez@gmail.com>
2017-08-30 19:56:15 -0700

    Chapter4 (#6)
    More updates to chapter 4 including web templates etc.

--- organizer/views.py
+++ organizer/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.template import Context, loader
 
-# Create your views here.
+from .models import Tag
+
+
+def homepage(request):
+    tag_list = Tag.objects.all()
+    #output = ",".join([tag.name for tag in tag_list])
+
+    context = {'tag_list': tag_list}
+    #context = Context({'tag_list': tag_list})  # Book wants this, but it's borked
+    template = loader.get_template('organizer/tag_list.html')
+
+    output = template.render(context)
+
+    return HttpResponse(output)
