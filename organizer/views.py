464984ef49c7e5850088b0ac3795c058fa5a40c9
Sal Gomez <svtgomez@gmail.com>
2017-12-04 23:48:27 -0800

    up to 5.8.2

--- organizer/views.py
+++ organizer/views.py
@@ -1,7 +1,7 @@
 #from django.shortcuts import render
 from django.http.response import Http404, HttpResponse, HttpResponseNotFound
-from django.shortcuts import get_object_or_404, render_to_response
-from django.template import Context, loader
+from django.shortcuts import get_object_or_404, render, render_to_response
+from django.template import RequestContext, loader
 
 from .models import Tag
 
@@ -16,7 +16,19 @@ def homepage(request):
     output = template.render(context)
     return HttpResponse(output)
     '''
-    return render_to_response('organizer/tag_list.html', {'tag_list': Tag.objects.all()})
+    '''
+    tag_list = Tag.objects.all()
+    template = loader.get_template('organizer/tag_list.html')
+    context = RequestContext(request, {'tag_list': tag_list})
+    output = template.render()
+    return HttpResponse(output)
+    '''
+
+    #return render_to_response('organizer/tag_list.html', {'tag_list': Tag.objects.all()})
+    return render(
+        request,
+        'organizer/tag_list.html',
+        {'tag_list': Tag.objects.all()})
 
 
 def tag_detail(request, slug):
@@ -28,7 +40,17 @@ def tag_detail(request, slug):
         raise Http404
     '''
     # Shortcut for this ^^^
-    tag = get_object_or_404(Tag, slug__iexact=slug)
+    tag = get_object_or_404(
+        Tag, slug__iexact=slug)
 
-    return render_to_response('organizer/tag_detail.html', {'tag': tag})
+    return render(
+        request,
+        'organizer/tag_detail.html',
+        {'tag': tag})
 
+
+def tag_list(request):
+    return render(
+        request,
+        'organizer/tag_list.html',
+        {'tag_list': Tag.objects.all()})
--- suorganizer/urls.py
+++ suorganizer/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from organizer.views import homepage, tag_detail
+from organizer import urls as organizer_urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^tag/(?P<slug>[\w\-]+)/$', tag_detail),
-    url(r'^$', homepage),
+    url(r'^', include(organizer_urls))
 ]
